b3b3dee2989ff6b619c8aad5ad7492e042a9a13d
Smashkat12 <smashkat12@gmail.com>
2019-10-29 12:07:37 +0200

    can now see timeline from index page

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
 include('./classes/dbh.php');
 include('./classes/login.inc.php');
-
+$showTimeline = false;
 if (Login::isLoggedin()) {
-	echo "Logged in";
-	echo Login::isLoggedin();
+	$showTimeline = true;
 } else {
 	echo "Not Logged in";
 }
+//Show the timeline
+$followingposts = DB::query('SELECT posts.body, posts.likes, users.`username` FROM users, posts, followers WHERE posts.user_id = followers.user_id AND users.id = posts.user_id AND follower_id = 7 ORDER BY posts.likes DESC;');
+foreach($followingposts as $post) {
+	echo $post['body']." ~ ".$post['username']."<hr />";
+}
+
 ?>
