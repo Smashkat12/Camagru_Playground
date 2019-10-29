8c11cfa9f951bd88bf99deaf65bbf58af2522387
Smashkat12 <smashkat12@gmail.com>
2019-10-29 12:47:25 +0200

    inc functionlaity to like posts on timeline

--- index.php
+++ index.php
@@ -8,9 +8,13 @@ if (Login::isLoggedin()) {
 	echo "Not Logged in";
 }
 //Show the timeline
-$followingposts = DB::query('SELECT posts.body, posts.likes, users.`username` FROM users, posts, followers WHERE posts.user_id = followers.user_id AND users.id = posts.user_id AND follower_id = 7 ORDER BY posts.likes DESC;');
+$followingposts = DB::query('SELECT posts.id, posts.body, posts.likes, users.`username` FROM users, posts, followers WHERE posts.user_id = followers.user_id AND users.id = posts.user_id AND follower_id = 7 ORDER BY posts.likes DESC;');
 foreach($followingposts as $post) {
-	echo $post['body']." ~ ".$post['username']."<hr />";
+	echo $post['body']." ~ ".$post['username'];
+	echo "<form action='index.php?postid=".$post['id']."' method='post'>
+	<input type='submit' name='like' value='Like'>
+	<span>".$post['likes']." likes</span>
+	</form>
+	<hr /></br />";
+	
 }
-
-?>
