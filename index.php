2cb9a4277c5912dc19b1c342d637bf2ecc20b716
Smashkat12 <smashkat12@gmail.com>
2019-10-30 08:05:48 +0200

    included comment box

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 include('./classes/dbh.php');
 include('./classes/login.inc.php');
 include('./classes/post.inc.php');
+include('./classes/comment.inc.php');
 
 $userid = Login::isLoggedin();
 $showTimeline = false;
@@ -10,14 +11,17 @@ if ($userid) {
 } else {
 	echo "Not Logged in";
 }
-if (isset($_GET['postid'])) {
-	Post::likePost($_GET['postid'], $userid);
+if (isset($_GET['postid2'])) {
+	Post::likePost($_GET['postid2'], $userid);
+}
+if (isset($_POST['comment'])) {
+	Comment::createComment($_POST['commentbody'], $_GET['postid'],$userid);
 }
 //Show the timeline
 $followingposts = DB::query('SELECT posts.id, posts.body, posts.likes, users.`username` FROM users, posts, followers WHERE posts.user_id = followers.user_id AND users.id = posts.user_id AND follower_id =:userid ORDER BY posts.likes DESC;', array(':userid'=>$userid));
 foreach($followingposts as $post) {
 		echo $post['body']." ~ ".$post['username'];
-		echo "<form action='index.php?postid=".$post['id']."' method='post'>";
+		echo "<form action='index.php?postid2=".$post['id']."' method='post'>";
 		if (!DB::query('SELECT post_id FROM post_likes WHERE post_id=:postid AND user_id=:userid', array(':postid'=>$post['id'], ':userid'=>$userid))) {
 			echo "<input type='submit' name='like' value='Like'>";
 		} else {
@@ -25,5 +29,11 @@ foreach($followingposts as $post) {
 		}
 		echo "<span>".$post['likes']." likes</span>
 		</form>
+		<form action='index.php?postid=".$post['id']."' method='post'>
+		<textarea name='commentbody' cols='50' rows='3'></textarea>
+		<input type='submit' name='comment' value='Comment'>
+		</form>
 		<hr /></br />";
+
+		
 }
