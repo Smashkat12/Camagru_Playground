5cc986f63e0a9fdb10754d49c6f88c62add1ee35
Smashkat12 <smashkat12@gmail.com>
2019-11-04 16:23:55 +0200

    fixed likes controlling which post is on top

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (isset($_POST['comment'])) {
 	Comment::createComment($_POST['commentbody'], $_GET['postid'],$userid);
 }
 //Show the timeline
-$followingposts = DB::query('SELECT posts.id, posts.body, posts.likes, users.`username` FROM users, posts, followers WHERE posts.user_id = followers.user_id AND users.id = posts.user_id AND follower_id =:userid ORDER BY posts.likes DESC;', array(':userid'=>$userid));
+$followingposts = DB::query('SELECT posts.id, posts.body, posts.posted_at, posts.likes, users.`username` FROM users, posts, followers WHERE posts.user_id = followers.user_id AND users.id = posts.user_id AND follower_id =:userid ORDER BY posts.posted_at DESC;', array(':userid'=>$userid));
 foreach($followingposts as $post) {
 		echo $post['body']." ~ ".$post['username'];
 		echo "<form action='index.php?postid2=".$post['id']."' method='post'>";
